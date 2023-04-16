bed9d73bf1e2cb06837027a00ed8b06760bf9626
yyamada12 <12yacropolisy@gmail.com>
2023-04-16 23:03:32 +0900

    initial commit

---
+++ src/main/kotlin/com/example/mybatisdemo/MybatisDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mybatisdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MybatisDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<MybatisDemoApplication>(*args)
+}
